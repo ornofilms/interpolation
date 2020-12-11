a1bf99983efedc1bd693c9c1f12e3a6c677523ec
Polina <polina.bystrevskaya@gmail.com>
2020-12-11 14:00:34 +0700

    add run-through method solving of slae, fix gauss_slae, cubic_spline_koeff

--- methods.py
+++ methods.py
@@ -47,16 +47,15 @@ def newtone_method(x, y, t, n, diff_storage):
 def gauss_slae(matrix, k):
     # forward stroke
     for iter in range(k):
-        imax = jmax = iter
+        imax = iter
+        jmax = 0
         for i in range(iter, k+1): # find maximum modulo value in all matrix
             for j in range(k+1):
                 if abs(matrix[i][j]) > abs(matrix[imax][jmax]):
                     imax = i
                     jmax = j
         for j in range(k+2):      # swap row witn max value and current-stap row
-            swap = matrix[iter][j]
-            matrix[iter][j] = matrix[imax][j]
-            matrix[imax][j] = swap
+            matrix[iter][j], matrix[imax][j] = matrix[imax][j], matrix[iter][j]
         c_max = matrix[iter][jmax]
         for j in range(k + 2):
             if c_max != 0:
@@ -72,11 +71,36 @@ def gauss_slae(matrix, k):
         b = matrix[i][-1]
         for j in range(k + 1):
             if j in solve.keys():
-                b -= solve[j]
+                b -= solve[j] * matrix[i][j]
         for j in range(k + 1):
             if matrix[i][j] != 0 and j not in solve.keys():
                 solve[j] = b / matrix[i][j]
     return solve
+
+
+def run_through_slae(a, b, c, d, n):
+    y = []
+    alpha = []
+    beta = []
+    # forward stroke
+    for i in range(n):
+        if i == 0:
+            y.append(b[i])
+            alpha.append(- c[i] / y[i])
+            beta.append(d[i] / y[i])
+        else:
+            y.append(b[i] + a[i] * alpha[i - 1])
+            beta.append((d[i] - a[i] * beta[i - 1]) / y[i])
+            if i < n - 1:
+                alpha.append(- c[i] / y[i])
+    x = {}
+    # reverse stroke
+    for i in reversed(range(n)):
+        if i == n - 1:
+            x[i] = beta[i]
+        else:
+            x[i] = alpha[i] * x[i + 1] + beta[i]
+    return x
 
 
 def koeff_a_least_square(x, y, n, k):
@@ -89,7 +113,7 @@ def koeff_a_least_square(x, y, n, k):
         for i in range(n):
             d[j] += y[i] * x[i] ** j
     matrix = []
-    for i in range(k + 1):  # формирование матрицы из значений с
+    for i in range(k + 1):  # forming a matrix from c values
         matrix.append([c[j + i] for j in range(k + 1)])
         matrix[i].append(d[i])
     return gauss_slae(matrix, k)
@@ -103,29 +127,29 @@ def least_square_method(a, t, k):
 
 
 def koeff_cubic_spline(x, y, n):
-    koeff = {'a': {}, 'b': {}, 'c': {}, 'd': {}}
-    matrix = [[0, 0, 0, 0] for i in range(n - 2)]
-    for j in range(n - 2):
-        i = j + 2
+    koeff = {'a': {}, 'b': {}, 'c': {}, 'd': {}}  # a-d koeff of cubic spline
+    a = []                                        # a-d for run-through method
+    b = []
+    c = []
+    d = []
+    for i in range(2, n):
+        j = i - 2
         hi_1 = x[i - 2] - x[i - 1]
         hi = x[i - 1] - x[i]
-        matrix[j][0] = hi_1
-        matrix[j][1] = 2 * (hi_1 + hi)
-        matrix[j][2] = hi if i < n else 0
-        matrix[j][3] = 3 * ((y[i] - y[i - 1]) / hi - (y[i - 1] - y[i - 2]) / hi_1)
-    c = gauss_slae(matrix, n - 3)
+        a.append(hi_1 if j != 0 else 0)
+        b.append(2 * (hi_1 + hi))
+        c.append(hi if i != n - 1 else 0)
+        d.append(3 * ((y[i] - y[i - 1]) / hi - (y[i - 1] - y[i - 2]) / hi_1))
+    c_koeff = run_through_slae(a, b, c, d, n - 2)
     koeff['c'][1] = 0
-    for i in c.keys():
-        koeff['c'][i + 2] = c[i]
+    for i in c_koeff.keys():
+        koeff['c'][i + 2] = c_koeff[i]
+    koeff['c'][n] = 0
     for i in range(1, n):
         h = x[i - 1] - x[i]
-        koeff['a'][i] = y[i]
-        if i < n - 1:
-            koeff['b'][i] = (y[i] - y[i - 1]) / h - (koeff['c'][i + 1] + 2 * koeff['c'][i] * h) / 3
-            koeff['d'][i] = (koeff['c'][i + 1] - koeff['c'][i]) / (3 * h)
-        else:
-            koeff['b'][i] = (y[i] - y[i - 1]) / h - (2 * koeff['c'][i] * h)
-            koeff['d'][i] = (- koeff['c'][i]) / (3 * h)
+        koeff['a'][i] = y[i - 1]
+        koeff['b'][i] = (y[i] - y[i - 1]) / h - (koeff['c'][i + 1] + 2 * koeff['c'][i]) * h / 3
+        koeff['d'][i] = (koeff['c'][i + 1] - koeff['c'][i]) / (3 * h)
     return koeff
 
 
@@ -134,7 +158,3 @@ def cubic_spline_method(x, koeff, t, n):
         if x[i - 1] <= t and t <= x[i]:
             return koeff['a'][i] + koeff['b'][i] * (t - x[i - 1]) + \
                    koeff['c'][i] * (t - x[i - 1])**2 + koeff['d'][i] * (t - x[i - 1])**3
-
-
-
-#print(gauss_slae([[1, -1, -5], [2, 1, -7]], 1))
